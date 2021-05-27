e10c640106edc98518c110cfc002f969f8fad6f1
xuyuliang <xuyuliang@gmail.com>
2021-05-27 17:50:55 +0800

    0527

--- sprawl.py
+++ sprawl.py
@@ -1,13 +1,15 @@
 import sys
-from PySide2 import QtWidgets,QtGui,QtCore
-import ui_sprawl
+from PySide2.QtWidgets import QMainWindow,QApplication
+from ui_sprawl import Ui_Form
 
-class SprawlWindow(ui_sprawl.Ui_Form,QtWidgets.QMainWindow):
-    def __init__(self,parent = None):
-        super(SprawlWindow,self).__init__(parent)
+class SprawlWindow(QMainWindow):
+    def __init__(self):
+        super(SprawlWindow,self).__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     wnd = SprawlWindow()
     wnd.show()
     app.exec_()
