556c0453a60e3f39a7d90895e09605cebb93ba11
Xu Yuliang <xuyuliang@gmail.com>
2021-06-06 22:19:57 +0800

    0606

--- sprawl.py
+++ sprawl.py
@@ -1,5 +1,8 @@
-#import processDB
+import processDB
 import sys
+
+from PyQt6.QtCore import pyqtSlot
+from PyQt6.QtSql import QSqlQueryModel
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from ui_sprawl import Ui_MainWindow
 
@@ -9,8 +12,17 @@ class MyMainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.test)
+        self.ui.pushButton_2.clicked.connect(self.on_pushButton2_clicked)
+        self.ui.tableView.setModel()
+
     def test(self):
-        self.ui.lineEdit_2.setText('你好，新的')
+        self.ui.lineEdit.setText('你好，新的')
+
+
+    @pyqtSlot(bool)
+    def on_pushButton2_clicked(self, value):
+        print("hello 你好")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
